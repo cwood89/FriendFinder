5bcdc137ddfe67cf1d9697dc81e92f3854ef77b1
woodtop <cwood89@mac.com>
2019-01-12 12:54:44 -0500

    heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,33 @@ var friends = [
       4,
       1
     ]
+  },
+  {
+    "name": "Mike",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      3,
+      1,
+      2,
+      4,
+      3,
+      1,
+      1,
+      5,
+      4,
+      3
+    ]
+  }, {
+    "name": "tracy",
+    "photo":
+      "https://medialicdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "score": [9, 4, 5, 9, 7, 8, 4, 1, 1, 9]
+  },
+  {
+    "name": "felecia",
+    "photo":
+      "https://media.licdn.,com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "score": [7, 1, 3, 8, 4, 6, 8, 5, 3, 3]
   }
 ];
 
