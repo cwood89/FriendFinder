4b3104b378a8fe7dcba28bd316c00e1840143d39
woodtop <cwood89@mac.com>
2019-02-08 15:03:12 -0500

    redoing project

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-// Dependencies
+
 var express = require("express");
 var path = require("path");
-// set up express server
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-// for data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//routes
+app.use(express.static("public"));
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-// start server
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
